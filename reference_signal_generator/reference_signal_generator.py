b04fc71d076778323b4738be038ac4cd6e8577a1
Victor T. N <victortn21@hotmail.com>
2024-06-06 14:23:46 +0000

    Node configured to multiple input signals

--- reference_signal_generator/reference_signal_generator.py
+++ reference_signal_generator/reference_signal_generator.py
@@ -1,17 +1,25 @@
 import rclpy
 from rclpy.node import Node
 
-from reference_signal_srvs.srv import Step
-from reference_signal_srvs.srv import Ramp
-from reference_signal_srvs.srv import Wave
-from reference_signal_srvs.srv import Chirp
+from reference_signal_srvs.srv import Setup
 
 from std_srvs.srv import Trigger
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64MultiArray
 
 from scipy import signal
 import math
+import array
 
+def check_array_length_consistency(arrays):
+    if not arrays:
+        return True
+    else:
+        length = len(arrays[0])
+        for arr in arrays:
+            if len(arr)!= length:
+                return False
+        return True
+    
 class RefSignalServer(Node):
     def __init__(self):
         # Initialize the node
@@ -19,57 +27,71 @@ class RefSignalServer(Node):
         # Declare the topic_name parameter
         self.declare_parameter('topic_name', 'reference_signal')
         topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
-        # Create the services for each signal type
-        self.step_service = self.create_service(Step, topic_name + '/step', self.step_callback)
-        self.ramp_service = self.create_service(Ramp, topic_name + '/ramp', self.ramp_callback)
-        self.square_service = self.create_service(Wave, topic_name +'/square', self.square_callback)
-        self.sine_service = self.create_service(Wave, topic_name +'/sine', self.sine_callback)
-        self.triangle_service = self.create_service(Wave, topic_name +'/triangle', self.triangle_callback)
-        self.sawtooth_service = self.create_service(Wave, topic_name +'/sawtooth', self.sawtooth_callback)
-        self.chirp_service = self.create_service(Chirp, topic_name +'/chirp', self.chirp_callback)
-        # Create an additional service to stop the reference signal
+        # Create services for setting up, starting and stopping the reference signal
+        self.setup_service = self.create_service(Setup, topic_name + '/setup', self.setup_callback)
+        self.start_service = self.create_service(Trigger, topic_name + '/start', self.start_callback)
         self.stop_service = self.create_service(Trigger, topic_name +'/stop', self.stop_callback)
         # Create a publisher with the parameterized topic name
-        self.publisher = self.create_publisher(Float64, topic_name, 10)
+        self.publisher = self.create_publisher(Float64MultiArray, topic_name, 10)
         # Initialize variables
-        self.msg = Float64()
-        self.msg.data = 0.0
+        self.msg = Float64MultiArray()
         self.publish_period = 0.0
         self.timer = self.create_timer(self.publish_period, self.publish_reference_signal)
-        self.elapsed_time = 0.0
+        self.elapsed_time = float('inf')
         self.total_time = 0.0
-        self.signal_type = 'none'
+        self.signal_type = []
 
-    def step_callback(self, request, response):
-        # Define signal type
-        self.signal_type = 'step'
-        # Read parameters from service call
-        self.initial_value = request.initial_value
-        self.final_value = request.final_value
-        self.step_time = request.step_time
-        self.publish_rate = request.publish_rate
-        self.publish_period = 1/self.publish_rate
-        self.total_time = request.total_time
-        # Destroy the previous timer
-        self.timer.destroy()
-        # Start the timer
-        self.elapsed_time = 0.0
-        self.timer = self.create_timer(self.publish_period, self.publish_reference_signal)
-        # Respond the service call
-        response.success = True
-        response.message = 'Step signal successfully started'
-        return response
+    def setup_callback(self, request, response):
+        # Check if provided signal types are valid
+        if any(s not in set(['step', 'ramp', 'sine', 'square', 'triangle', 'sawtooth', 'chirp']) for s in request.signal_type):
+            response.success = False
+            response.message = 'Invalid signal type detected'
+            return response
+        # Check if all parameter arrays have the same length
+        elif not check_array_length_consistency([request.signal_type,
+                                                 request.initial_value,
+                                                 request.final_value,
+                                                 request.start_time,
+                                                 request.end_time,
+                                                 request.slope,
+                                                 request.offset,
+                                                 request.amplitude,
+                                                 request.frequency,
+                                                 request.phase,
+                                                 request.initial_frequency,
+                                                 request.target_frequency,
+                                                 request.target_time]):
+            response.success = False
+            response.message = 'Parameter array length inconsistency detected'
+            return response
+        else:
+            # Define signal type
+            self.signal_type = request.signal_type
+            # Check if parameter arrays match the length of signal_type array
+            # Read signal parameters from service call
+            self.initial_value = request.initial_value
+            self.final_value = request.final_value
+            self.start_time = request.start_time
+            self.end_time = request.end_time
+            self.slope = request.slope
+            self.offset = request.offset
+            self.amplitude = request.amplitude
+            self.frequency = request.frequency
+            self.phase = request.phase
+            self.initial_frequency = request.initial_frequency
+            self.target_frequency = request.target_frequency
+            self.target_time = request.target_time
+            self.publish_rate = request.publish_rate
+            self.publish_period = 1/self.publish_rate
+            self.total_time = float(request.total_time)
+            # Initialize msg.data
+            self.msg.data = array.array('d', [0.0] * len(self.signal_type))
+            # Respond the service call
+            response.success = True
+            response.message = 'Reference signal successfully setup'
+            return response
     
-    def ramp_callback(self, request, response):
-        # Define signal type
-        self.signal_type = 'ramp'
-        # Read parameters from service call
-        self.initial_value = request.initial_value
-        self.slope = request.slope
-        self.start_time = request.start_time
-        self.publish_rate = request.publish_rate
-        self.publish_period = 1/self.publish_rate
-        self.total_time = request.total_time
+    def start_callback(self, request, response):
         # Destroy the previous timer
         self.timer.destroy()
         # Start the timer
@@ -77,85 +99,7 @@ class RefSignalServer(Node):
         self.timer = self.create_timer(self.publish_period, self.publish_reference_signal)
         # Respond the service call
         response.success = True
-        response.message = 'Ramp signal successfully started'
-        return response
-
-    def square_callback(self, request, response):
-        # Define signal type
-        self.signal_type = 'square'
-        # Call the waveform callback
-        self.wave_callback(request)
-        # Respond the service call
-        response.success = True
-        response.message = 'Square signal successfully started'
-        return response
-    
-    def sine_callback(self, request, response):
-        # Define signal type
-        self.signal_type = 'sine'
-        # Call the waveform callback
-        self.wave_callback(request)
-        # Respond the service call
-        response.success = True
-        response.message = 'Sine signal successfully started'
-        return response
-
-    def triangle_callback(self, request, response):
-        # Define signal type
-        self.signal_type = 'triangle'
-        # Call the waveform callback
-        self.wave_callback(request)
-        # Respond the service call
-        response.success = True
-        response.message = 'Triangle signal successfully started'
-        return response
-
-    def sawtooth_callback(self, request, response):
-        # Define signal type
-        self.signal_type = 'sawtooth'
-        # Call the waveform callback
-        self.wave_callback(request)
-        # Respond the service call
-        response.success = True
-        response.message = 'Sawtooth signal successfully started'
-        return response
-    
-    # Generic wave callback (used by square, sine, triangle and sawtooth)
-    # All four use the same Wave.srv service interface
-    def wave_callback(self, request):
-        # Read parameters from service call
-        self.offset = request.offset
-        self.amplitude = request.amplitude
-        self.frequency = request.frequency
-        self.publish_rate = request.publish_rate
-        self.publish_period = 1/self.publish_rate
-        self.total_time = request.total_time
-        # Destroy the previous timer
-        self.timer.destroy()
-        # Start the timer
-        self.elapsed_time = 0.0
-        self.timer = self.create_timer(self.publish_period, self.publish_reference_signal)
-
-    def chirp_callback(self, request, response):
-        # Define signal type
-        self.signal_type = 'chirp'
-        # Read parameters from service call
-        self.offset = request.offset
-        self.amplitude = request.amplitude
-        self.initial_frequency = request.initial_frequency
-        self.target_frequency = request.target_frequency
-        self.target_time = request.target_time
-        self.publish_rate = request.publish_rate
-        self.publish_period = 1/self.publish_rate
-        self.total_time = request.total_time
-        # Destroy the previous timer
-        self.timer.destroy()
-        # Start the timer
-        self.elapsed_time = 0.0
-        self.timer = self.create_timer(self.publish_period, self.publish_reference_signal)
-        # Respond the service call
-        response.success = True
-        response.message = 'Chirp signal successfully started'
+        response.message = 'Reference signal successfully started'
         return response
 
     def stop_callback(self, request, response):
@@ -164,9 +108,7 @@ class RefSignalServer(Node):
         self.elapsed_time = float('inf')
         # Respond the service call
         response.success = True
-        response.message = self.signal_type.capitalize() + ' signal successfully stopped'
-        # Set signal_type to 'none'
-        self.signal_type = 'none'
+        response.message = 'Reference signal successfully stopped'
         return response
 
     def publish_reference_signal(self):
@@ -174,36 +116,53 @@ class RefSignalServer(Node):
             # Stop publishing
             self.timer.destroy()
             self.elapsed_time = float('inf')
-            self.signal_type = 'none'
         else:
-            # Recognize the signal type
-            match self.signal_type:
-                # Generate the reference signal at current timestamp
-                case'step':
-                    if self.elapsed_time >= self.step_time:
-                        self.msg.data = self.final_value
-                    else:
-                        self.msg.data = self.initial_value
-                case 'ramp':
-                    if self.elapsed_time >= self.start_time:
-                        self.msg.data = self.initial_value + (self.elapsed_time - self.start_time)*self.slope
-                    else:
-                        self.msg.data = self.initial_value
-                case 'square':
-                    self.msg.data = self.offset + self.amplitude/2 * math.copysign(1, math.sin(2 * math.pi * self.frequency * self.elapsed_time))
-                case 'sine':
-                    self.msg.data = self.offset + self.amplitude/2 * math.sin(2 * math.pi * self.frequency * self.elapsed_time)
-                case 'triangle':
-                    self.msg.data = self.offset + self.amplitude/2 * signal.sawtooth(2 * math.pi * self.frequency * self.elapsed_time, width=0.5)
-                case 'sawtooth':
-                    self.msg.data = self.offset + self.amplitude/2 * signal.sawtooth(2 * math.pi * self.frequency * self.elapsed_time, width=1)
-                case 'chirp':
-                    self.msg.data = self.offset + self.amplitude/2 * signal.chirp(self.elapsed_time, self.initial_frequency, self.target_time, self.target_frequency)
-                case _:
-                    pass
-            # Publish the message
+            # Check each reference input
+            for i in range(len(self.signal_type)):
+                # Recognize the signal type
+                match self.signal_type[i]:
+                    case 'step':
+                        if self.elapsed_time >= self.start_time[i] and self.elapsed_time < self.end_time[i]:
+                                self.msg.data[i] = self.final_value[i]
+                        else:
+                            self.msg.data[i] = self.initial_value[i]
+                    case 'ramp':
+                        if self.elapsed_time < self.start_time[i]:
+                            self.msg.data[i] = self.initial_value[i]
+                        elif self.elapsed_time < self.end_time[i]:
+                            self.msg.data[i] = self.initial_value[i] + (self.elapsed_time - self.start_time[i])*self.slope[i]
+                        else:
+                            self.msg.data[i] = self.initial_value[i] + (self.end_time[i] - self.start_time[i])*self.slope[i]
+                    case 'sine':
+                        if self.elapsed_time >= self.start_time[i] and self.elapsed_time < self.end_time[i]:
+                            self.msg.data[i] = self.offset[i] + self.amplitude[i]/2 * math.sin(2 * math.pi * self.frequency[i] * (self.elapsed_time - self.start_time[i]) + self.phase[i])
+                        else:
+                            self.msg.data[i] = self.offset[i]
+                    case 'square':
+                        if self.elapsed_time >= self.start_time[i] and self.elapsed_time < self.end_time[i]:
+                            self.msg.data[i] = self.offset[i] + self.amplitude[i]/2 * math.copysign(1, math.sin(2 * math.pi * self.frequency[i] * (self.elapsed_time - self.start_time[i]) + self.phase[i]))
+                        else:
+                            self.msg.data[i] = self.offset[i]
+                    case 'triangle':
+                        if self.elapsed_time >= self.start_time[i] and self.elapsed_time < self.end_time[i]:
+                            self.msg.data[i] = self.offset[i] + self.amplitude/2 * signal.sawtooth(2 * math.pi * self.frequency[i] * (self.elapsed_time - self.start_time[i]) + self.phase[i], width=0.5)
+                        else:
+                            self.msg.data[i] = self.offset[i]
+                    case 'sawtooth':
+                        if self.elapsed_time >= self.start_time[i] and self.elapsed_time < self.end_time[i]:
+                            self.msg.data[i] = self.offset[i] + self.amplitude/2 * signal.sawtooth(2 * math.pi * self.frequency[i] * (self.elapsed_time - self.start_time[i]) + self.phase[i], width=1)
+                        else:
+                            self.msg.data[i] = self.offset[i]
+                    case 'chirp':
+                        if self.elapsed_time >= self.start_time[i] and self.elapsed_time < self.end_time[i]:
+                            self.msg.data[i] = self.offset[i] + self.amplitude[i]/2 * signal.chirp(self.elapsed_time - self.start_time[i], self.initial_frequency[i], self.target_time[i], self.target_frequency[i], phi=self.phase[i])
+                        else:
+                            self.msg.data[i] = self.offset[i]
+                    case _:
+                        pass
+            # Publish the reference signal
             self.publisher.publish(self.msg)
-            # Increment the elapsed time
+            # Update the elapsed time
             self.elapsed_time += self.publish_period
 
 def main():
